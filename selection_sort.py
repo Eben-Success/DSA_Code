e50f4a2cc96063eaeb29f7730a108559672b6a7d
eben-success <aebenezer237@gmail.com>
2022-09-30 10:36:30 +0000

    update

--- selection_sort.py
+++ selection_sort.py
@@ -9,5 +9,9 @@ def findSmallest(arr):
 
     def selectionSort(arr):
         newArr = []
+        for i in range(len(arr)):
+            smallest = findSmallest(arr)
+            newArr.append(arr.pop(smallest))
+        return newArr
 
-        
+    print(selectionSort([5,3,6,2,10]))
